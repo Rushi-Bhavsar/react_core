dd3c85af41466ce05ca551114074f6840427407a
Rushi-Bhavsar <rushi.bhavsar.57@gmail.com>
2023-03-21 20:44:54 +0530

    Code Optimization using arrow function. Adding Topic file to track topic covered.

---
+++ Topics.md
@@ -0,0 +1,8 @@
+## React Topic
+1. NPM and NPX
+2. Project Folder structure.
+3. What is JSX syntax.
+4. What is React component.
+5. What is PROPS, PROPS-Type and Default PROPS.
+6. What is State.
+7. 
--- src/components/getSetState.js
+++ src/components/getSetState.js
@@ -3,22 +3,36 @@ import PropTypes from "prop-types";
 
 const TextForm = (props) => {
 
-    function convertUpperCase() {
-        console.log(text)
-        setText(text.toUpperCase())
-    }
+    let textWords;
 
     const [text, setText] = useState(props.initialMessage)
+    const textLength = text.length
+    if (text){
+        textWords = text.split(" ").length;
+    }
+    else{
+        textWords = 0
+    }
 
     return (
-       <div className={'container'}>
-           <div className="mb-3">
-               <h2>{props.heading}</h2>
-               <textarea className="form-control" value={text} onChange={(event) => {setText(event.target.value)}}
-                         id="mybox" rows="10"></textarea>
+       <>
+           <div className={'container'}>
+               <div className="mb-3">
+                   <h2>{props.heading}</h2>
+                   <textarea className="form-control" value={text} onChange={(event) => {setText(event.target.value)}}
+                             id="mybox" rows="10"></textarea>
+               </div>`
+               <button className="btn btn-primary" onClick={() => {setText(text.toUpperCase())}}>UpperText</button>
+               <button className="btn btn-primary mx-1" onClick={() => {setText(text.toLowerCase())}}>LowerText</button>
            </div>
-           <button className="btn btn-primary" onClick={convertUpperCase}>UpperText</button>
-       </div>
+           <div className="container my-4">
+               <h2>Text Summary</h2>
+               <p>{textWords} words and {textLength} characters</p>
+               <p>{textWords * 0.008} Min are required to read.</p>
+               <h2>Text Preview</h2>
+               <p>{text}</p>
+           </div>
+       </>
     );
 };
 export default TextForm;
